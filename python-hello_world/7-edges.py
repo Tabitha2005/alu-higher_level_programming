299a8ed906e6303fedce0e256556f8cfce1fce3e
Tabitha2005 <t.kuir@alustudent.com>
2024-10-24 11:56:03 -0700

    7-edges.py

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+first_three = str1[:3]
+last_two = str1[-2:]
+middle_word = str1[3:-2]
+print(f"First 3 letters: {first_three}")
+print(f"Last 2 letters: {last_two}")
+print(f"Middle word: {middle_word}")
