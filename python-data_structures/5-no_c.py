e8690a9e4201fda930e5f2a0e230cc4b8eb2845a
Tabitha2005 <t.kuir@alustudent.com>
2024-10-25 06:58:27 -0700

    5-no_c.py

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    result = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            result += char
+    return result
+
+if __name__ == "__main__":
+    print(no_c("Best School"))
+    print(no_c("Chicago"))
+    print(no_c("C is fun!"))
