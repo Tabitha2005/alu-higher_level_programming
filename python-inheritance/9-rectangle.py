0c26a2d5a8e07c56fe3eeccb02a38e5f7a371c5a
Tabitha2005 <t.kuir@alustudent.com>
2024-11-09 08:27:32 -0800

    8-rectangle.py

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,69 +1,21 @@
 #!/usr/bin/python3
-'''Class Rectangle that inherits from BaseGeometry'''
+'''Class Square that inherits from Rectangle'''
+
+Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Rectangle:
-    '''Rectangle class that inherits BaseGeometry'''
+class Square(Rectangle):
+    '''Square class that inherits from Rectangle'''
 
-    def __init__(self, width=0, height=0):
-        '''A function that creates a rectangle '''
-        self.integer_validator('width', width)
-        self.integer_validator('height', height)
-        self.__width = width
-        self.__height = height
+    def __init__(self, size):
+        '''Initialize a new square.
 
-    def area(self):
-        '''Function to return the area of the rectangle'''
-        return self.__width * self.__height
+        Args:
+            size (int): The size of the new square.
+        '''
+        self.integer_validator('size', size)
+        super().__init__(size, size)
 
     def __str__(self):
-        '''Return the print() and str() representation of a Rectangle '''
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
-    def integer_validator(self, name, value):
-        '''Validate the input'''
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-if __name__ == "__main__":
-    # Test cases
-    print(dir(Rectangle))
-
-    r = Rectangle(1, 4)
-    print(r.area())
-
-    r = Rectangle(1411, 781)
-    print(r.area())
-
-    r = Rectangle(5, 5)
-    print(r.area())
-
-    try:
-        r = Rectangle(1411, 781)
-        print(r.area())
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        r = Rectangle(1, 4)
-        print(r.area())
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        r = Rectangle(5, 5)
-        print(r.area())
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        r = Rectangle()
-        r = Rectangle(1)
-        r = Rectangle(1, [12, 52])
-        r = Rectangle(4, 5)
-        r = Rectangle(4, 5)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+        '''Return the print() and str() representation of a Square'''
+        return "[Square] {}/{}".format(self.__width, self.__height)
