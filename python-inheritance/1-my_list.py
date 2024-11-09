f3b1a25da732c37e1b9b2ce4a6fb278d6d6eda7b
Tabitha2005 <t.kuir@alustudent.com>
2024-11-09 10:35:56 -0800

    1-my_list.

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,67 +1,18 @@
->>> MyList = _import_('1-my_list').MyList
+#!/usr/bin/python3
+"""Defines a class Rectangle that inherits from BaseGeometry."""
+BaseGeometry = _import_('7-base_geometry').BaseGeometry
 
 
->>> my_list = MyList()
->>> my_list.append(1)
->>> my_list
-[1]
->>> my_list.append(2)
->>> my_list
-[1, 2]
->>> my_list.pop()
-2
->>> my_list
-[1]
->>> my_list.append(4)
->>> my_list.append(6)
->>> my_list.append(3)
->>> my_list.print_sorted()
-[1, 3, 4, 6]
->>> my_list
-[1, 4, 6, 3]
+class Rectangle(BaseGeometry):
+    """Represent a rectangle using BaseGeometry."""
 
->>> my_list.clear()
->>> my_list.append(1)
->>> my_list.append(5)
->>> my_list.append(8)
->>> my_list.append(7)
->>> my_list.append(9)
->>> my_list.append(2)
->>> my_list.append(0)
->>> new_l = my_list.copy()
->>> new_l
-[1, 5, 8, 7, 9, 2, 0]
->>> my_list.count(2)
-1
->>> my_list.index(3)
-Traceback (most recent call last):
-        ...
-ValueError: 3 is not in list
-
->>> my_list.pop(3)
-7
->>> my_list
-[1, 5, 8, 9, 2, 0]
-
->>> my_list.insert(9, 0)
->>> my_list
-[1, 5, 8, 9, 2, 0, 0]
-
->>> my_list.pop(20)
-Traceback (most recent call last):
-        ...
-IndexError: pop index out of range
-
->>> my_list.print_sorted(8)
-Traceback (most recent call last):
-        ...
-TypeError: print_sorted() takes 1 positional argument but 2 were given
-
->>> my_list.clear()
->>> my_list.print_sorted()
-[]
->>> my_list.append(9)
->>> my_list.append(-2)
->>> my_list.append(8)
->>> my_list.print_sorted()
-[-2, 8, 9]
+    def _init_(self, width, height):
+        """Intialize a new Rectangle.
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
