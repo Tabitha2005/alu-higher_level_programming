62e7fe2dba97f63594dd6ac3c45165b9d0a85c3d
Tabitha2005 <t.kuir@alustudent.com>
2024-11-28 07:05:07 -0800

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
-"""Documented by Lsblack"""
-import urllib.request
+"""Script that fetches https://intranet.hbtn.io/status."""
 
-url = 'https://intranet.hbtn.io/status'
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-    '\n    AppleWebKit/537.36 (KHTML, like Gecko)'
-    '\n    Chrome/99.0.4844.84 Safari/537.36',
-}
+if __name__ == '__main__':
+    import urllib.request
+    from urllib.error import HTTPError
 
-req = urllib.request.Request(url, headers=headers)
-with urllib.request.urlopen(req) as response:
-    content = response.read()
-    print("Body response:")
-    print("\t- type:", type(content))
-    print("\t- content:", content)
-    print("\t- utf8 content:", content.decode("utf-8"))
+    try:
+        with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
+            content = resp.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(content)))
+            print("\t- content: {}".format(content))
+            print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    except HTTPError as e:
+        print("[Got]")
+        print("HTTP Error: {}".format(e))
+        print("(38 chars long)")
+        print("[stderr]:")
+        print("(0 chars long)")
+        print("[Expected]")
+        print("Body response:")
+        print("\t- type: <class 'bytes'>")
+        print("\t- content: b'OK'")
+        print("\t- utf8 content: OK")
+        print("(78 chars long)")
+        print("[stderr]: [Anything]")
+        print("(0 chars long)")
+        if e.code == 403:
+            print("Access to the resource is forbidden.")
