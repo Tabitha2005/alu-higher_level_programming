fa28d92a83730bfde0126140cf5c4d9e80a0abc6
Tabitha2005 <t.kuir@alustudent.com>
2024-12-01 06:31:26 -0800

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,30 +1,20 @@
 #!/usr/bin/python3
-"""Script that fetches https://intranet.hbtn.io/status."""
+"""
+Documentation
+Fetches data from the url using
+the urllib module in python
+"""
+
+import urllib.request
+
+url = 'https://intranet.hbtn.io/status'
+if url.startswith('https://'):
+    url = 'https://alu-intranet.hbtn.io/status'
 
 if __name__ == '__main__':
-    import urllib.request
-    from urllib.error import HTTPError
-
-    try:
-        with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
-            content = resp.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(content)))
-            print("\t- content: {}".format(content))
-            print("\t- utf8 content: {}".format(content.decode('utf-8')))
-    except HTTPError as e:
-        print("[Got]")
-        print("HTTP Error: {}".format(e))
-        print("(38 chars long)")
-        print("[stderr]:")
-        print("(0 chars long)")
-        print("[Expected]")
+    with urllib.request.urlopen(url) as f:
+        content = f.read()
         print("Body response:")
-        print("\t- type: <class 'bytes'>")
-        print("\t- content: b'OK'")
-        print("\t- utf8 content: OK")
-        print("(78 chars long)")
-        print("[stderr]: [Anything]")
-        print("(0 chars long)")
-        if e.code == 403:
-            print("Access to the resource is forbidden.")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 c
