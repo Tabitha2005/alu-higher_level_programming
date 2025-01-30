ffb114a0c22dcafc0071edf0e270c4fe52610627
Aluel Tabitha Kuir <t.kuir@alustudent.com>
2025-01-30 12:43:43 +0200

    Successfully deleted

--- javascript-warm_up/11-second_biggest.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const argArray = process.argv.slice(2);
-if (argArray.length < 2) {
-  console.log(0);
-} else {
-  argArray.sort((a, b) => (a - b));
-  argArray.pop();
-  console.log(argArray.pop());
-}
